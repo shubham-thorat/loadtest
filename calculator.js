839fa1a4e725fb2a9c6ef293b719e0f648a235ca
Ubuntu <ubuntu@ip-172-31-0-64.ap-south-1.compute.internal>
2023-09-13 19:33:40 +0000

    output for machine1 added

--- calculator.js
+++ calculator.js
@@ -1,13 +1,14 @@
 import fs from 'fs'
 
 function addToJSON(data){
-  const output_file = 'ws_output.json'
+  const output_file = 'ws_output_m1.json'
   let outputjson = fs.readFileSync(output_file, "utf-8");
   if (outputjson === '') {
     outputjson = '[]'
   }
   let result = JSON.parse(outputjson)
   // console.log("outputFile", result)
+  data['test_no'] = result.length
   result.push(data)
   fs.writeFileSync(output_file, JSON.stringify(result, null, 2), 'utf-8')
 
